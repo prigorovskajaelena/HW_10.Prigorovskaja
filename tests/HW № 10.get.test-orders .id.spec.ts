06993fa08b8b71a4c0d36875c7cd939706174d83
Elena <prigorovskajaelena@gmail.com>
2026-04-07 09:36:13 +0300

    add API tests for GET, PUT, DELETE

--- tests/HW № 10.get.test-orders .id.spec.ts
+++ tests/HW № 10.get.test-orders .id.spec.ts
@@ -14,17 +14,14 @@ test('get order with incorrect id=0 should receive code 400', async ({ request }
 test('get order with incorrect id=11 should receive code 400', async ({ request }) => {
   // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
-  // parse raw response body to json
   const responseBody = await response.json()
   const statusCode = response.status()
   console.log('response body:', responseBody)
-  // Check if the response status is 200
   expect(statusCode).toBe(400)
 });
 
 test('get order with incorrect id=ABCD should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/ABCD')
-  // parse raw response body to json
   const responseBody = await response.json()
   const statusCode = response.status()
   console.log('response body:', responseBody)
@@ -36,7 +33,7 @@ test('get order with incorrect id=ABCD should receive code 400', async ({ reques
   //я передаю числа, а тест не выдает ошибку 400, а наоборот говорит,
   // что должно быть 200..
   //Вот в этом тесте
-  //HW № 10.get.test-orders{u.name,passw.}.spec.ts
+  //HW № 10.get.test-orders.u.name,passw.spec.ts
   // test('get order with incorrect name & correct password  should receive code 400')
 });
 
@@ -48,7 +45,7 @@ test('get order with incorrect id=" " should receive code 400', async ({ request
   const statusCode = response.status()
   console.log('response body:', responseBody)
   expect(statusCode).toBe(400)
-}); //Тест упал: Expected: 400, Received: 500
+}); //Тест упал: Expected: 400, Received: 405
 
 test('get order with incorrect id= symbols "[]{}@£=" should receive code 400', async ({ request }) => {
   // Build and send a GET request to the server
