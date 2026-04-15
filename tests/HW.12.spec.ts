78522cc0195317b133a5d7615461b732bb8738ad
Elena <prigorovskajaelena@gmail.com>
2026-04-15 23:30:28 +0300

    changes

--- tests/HW.12.spec.ts
+++ tests/HW.12.spec.ts
@@ -4,23 +4,21 @@ import { loginDTO } from '../src/dto/loginDTO'
 test.describe('login api tests', () => {
   const baseEndPointURL = 'https://backend.tallinn-learning.ee/login/student'
 
-test('incorrect login', async ({ request }) => {
-  const loginResponse = await request.post(baseEndPointURL, {
-    data: loginDTO.generateIncorrectPair(),
-  })
-  expect(loginResponse.status()).toBe(401)
-})
-
-test('correct login', async ({ request }) => {
-  console.log(loginDTO.generateCorrectPair())
-  const loginResponse = await request.post(baseEndPointURL, {
-    data: loginDTO.generateCorrectPair(),
+  test('incorrect login', async ({ request }) => {
+    const loginResponse = await request.post(baseEndPointURL, {
+      data: loginDTO.generateIncorrectPair(),
+    })
+    expect(loginResponse.status()).toBe(401)
   })
 
-  const token=await loginResponse.text()
-  expect(loginResponse.status()).toBe(200)
-  expect (token.length).toBeGreaterThan(0)
-})
+  test('correct login', async ({ request }) => {
+    console.log(loginDTO.generateCorrectPair())
+    const loginResponse = await request.post(baseEndPointURL, {
+      data: loginDTO.generateCorrectPair(),
+    })
 
-
+    const token = await loginResponse.text()
+    expect(loginResponse.status()).toBe(200)
+    expect(token.length).toBeGreaterThan(0)
+  })
 })
